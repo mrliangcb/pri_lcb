acbf088547d69c864d88ab603a01c42b4c18ccb5
mrliangcb <466312597@qq.com>
2020-11-05 15:10:39 +0800

    123

--- client_match_url.py
+++ client_match_url.py
@@ -1,6 +1,6 @@
 import requests
 
-url='http://127.0.0.1:50000/NLP/Algorithm/base/dup_check/template_match'
+url='http://127.0.0.1:50000/NLP/Algorithm/base/dup_check/template_match?source=1&template=2'
 res=requests.(url)
 
 

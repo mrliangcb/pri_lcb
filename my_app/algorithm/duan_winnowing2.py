faee2fcd768f8177f5e70db84281c015751d9ef6
liang_cb <18826453828@163.com>
2020-11-25 13:12:12 +0800

    123

--- my_app/algorithm/duan_winnowing2.py
+++ my_app/algorithm/duan_winnowing2.py
@@ -15,8 +15,8 @@ class paragraph_winnowing():
         if x1==x2:
             similarity=1
             doc1_str=0
-            doc1_wrap=[[(0,0,len(x1))]]
-            doc2_wrap = [[(0, 0, len(x2))]]
+            doc1_wrap=[[(0,0,len(x1)-1)]]
+            doc2_wrap = [[(0,0,len(x2)-1)]]
             return similarity,doc1_str,doc1_wrap,doc2_wrap
 
 
@@ -92,7 +92,7 @@ class paragraph_winnowing():
         # 正常
         # print('doc2_group_index在这里',doc2_group_index)
         doc2_wrap = self.doc2_tuple(doc2_group_index)
-        # print('doc2_wrap:',doc2_wrap)  #[[(1, 0, 29), (-1, 30, 38), (0, 39, 68), (-1, 69, 74), (0, 75, 112)]]
+        # print('doc2_wrap:',doc2_wrap)  #[[(1, 0, 29),(-1, 30, 38), (0, 39, 68), (-1, 69, 74), (0, 75, 112)]]
 
         #按句号截取，然后计算每部分的重复率，写入字典，并写入top k堆
 
@@ -117,7 +117,6 @@ class paragraph_winnowing():
         #         doc1_wrap[0][i]=tuple([-1,b,c])
         #         print('验证不合格的组号:',a)
         # print('验证及格后的doc1_wrap:',doc1_wrap)
-
         return similarity,doc1_str,doc1_wrap,doc2_wrap
 
     def compu_dup_rate(self,doc1_wrap,size):
